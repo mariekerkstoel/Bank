bf57b5d4586ed07873ee241e80575d98c880d72b
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-02 10:57:22 +0000

    Added Account class, tested for initial balance of 0, tests passing

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,1 +1,11 @@
+require 'spec_helper'
+require 'account'
 
+describe Account do
+  let (:account) {described_class.new}
+  describe '#balance' do
+    it 'should show me an initial balance of 0' do
+      expect(account.balance).to eq(0)
+    end
+  end
+end
