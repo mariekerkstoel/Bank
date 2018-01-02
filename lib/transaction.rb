09d8ce9e00941b9da23098432b9e6d10e73b6ee9
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-02 14:39:59 +0000

    Add create_action method that creates deposit or withdrawal instance depending on type

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -3,9 +3,15 @@ require './lib/deposit.rb'
 require './lib/withdrawal.rb'
 
 class Transaction
-  attr_reader :date
+  attr_reader :date, :action
 
   def initialize(amount, type)
     @date = Time.new
+    create_action(amount, type)
+  end
+
+  def create_action(amount, type)
+    @action = Deposit.new(amount) if type == 'Deposit'
+    @action = Withdrawal.new(amount) if type == 'Withdrawal'
   end
 end
