42b29ec9534053eb485d47725773be778f3542e7
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-02 14:40:19 +0000

    Test create action method

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -3,9 +3,22 @@ require 'spec_helper'
 
 describe Transaction do
   let(:transaction) { described_class.new(5, 'Deposit') }
-  describe 'date' do
+  describe 'initialize' do
     it 'every transaction should have a date' do
       expect(transaction.date).to be_truthy
+    end
+  end
+  describe '#create_action' do
+    it 'should take 2 arguments' do
+      expect { transaction.create_action }.to raise_error
+    end
+    it 'should create a deposit if type is deposit' do
+      transaction.create_action(7, 'Deposit')
+      expect(transaction.action).to be_an_instance_of Deposit
+    end
+    it 'should create a withdrawal if type is withdrawal' do
+      transaction.create_action(7, 'Withdrawal')
+      expect(transaction.action).to be_an_instance_of Withdrawal
     end
   end
 end
