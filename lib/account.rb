833cd5812c3283b2cfc32c553ce3d316beb42ce1
Marie Kerkstoel <mariekerkstoel@gmail.com>
2018-01-02 11:05:09 +0000

    Added option to initialize account with a balance other than 0, tested, tests passing

--- lib/account.rb
+++ lib/account.rb
@@ -1,6 +1,6 @@
 class Account
   attr_reader :balance
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 require 'account'
 
 describe Account do
-  let (:account) {described_class.new}
+  let (:account) {described_class.new()}
+  let (:account1) {described_class.new(64)}
   describe '#balance' do
     it 'should show me an initial balance of 0' do
       expect(account.balance).to eq(0)
+    end
+    it 'should be able to initialize with a balance other than 0' do
+      expect(account1.balance).to eq(64)
     end
   end
 end
